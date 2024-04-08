68c7c04c777c66db6cac35374ab4049ddc37cadb
Smasky <492109831@qq.com>
2024-04-08 23:20:12 +0800

    '0408'

--- UQPyL/Optimization/_binary_ga.py
+++ UQPyL/Optimization/_binary_ga.py
@@ -12,8 +12,9 @@ class Binary_GA():
         return np.random.randint(2, size=(self.population_size, self.n_features))
     
     def select(self, fitnesses):
-        total_fitness = sum(fitnesses)
-        selection_probs = [f/total_fitness for f in fitnesses]
+        inverse_fitnesses = [1.0/f for f in fitnesses]
+        total_fitness = sum(inverse_fitnesses)
+        selection_probs = [f/total_fitness for f in inverse_fitnesses]
         return np.random.choice(range(self.population_size), size=self.population_size, replace=True, p=selection_probs)
 
     def crossover(self, parent1, parent2):
@@ -32,8 +33,19 @@ class Binary_GA():
     
     def run(self):
         population = self.initialize_population()
+        best_individual = None
+        best_fitness = float('inf')
+        history_individuals = []
+        history_values=[]
         for generation in range(self.n_generations):
             fitnesses = [self.evaluate(individual) for individual in population]
+            if min(fitnesses) < best_fitness:
+                best_fitness = min(fitnesses)
+                best_individual = population[np.argmin(fitnesses)]
+                
+            history_individuals.append(best_individual)
+            history_values.append(best_fitness)
+                
             selected_indices = self.select(fitnesses)
             selected_population = population[selected_indices]
             offspring_population = []
@@ -43,5 +55,7 @@ class Binary_GA():
                 offspring_population.append(self.mutate(child1))
                 offspring_population.append(self.mutate(child2))
             population = np.array(offspring_population)
-        best_individual_index = np.argmin(fitnesses)
-        return population[best_individual_index], min(fitnesses)
+            # 精英保留
+            if best_individual is not None:
+                population[0] = best_individual
+        return best_individual, best_fitness, history_individuals, history_values
--- UQPyL/Sensitivity_Analysis/delta_test.py
+++ UQPyL/Sensitivity_Analysis/delta_test.py
@@ -2,18 +2,25 @@
 import numpy as np
 from ..Experiment_Design import LHS
 from ..Optimization import Binary_GA
+from .      .Utility import MinMaxScaler
 from scipy.spatial.distance import cdist
 
 lhs=LHS('center')
+scale=MinMaxScaler(0,1)
 class DELTA_TEST():
-    def __init__(self, problem, NSample=1000, n_neighbors=2):
+    def __init__(self, problem, NSample=1000, n_neighbors=2, surrogate=None, NSurrogate=50, XInit=None, YInit=None):
         self.lb=problem.lb; self.ub=problem.ub
         self.NInput=problem.dim
         self.evaluator=problem.evaluate
         self.n_neighbors=n_neighbors
         
         self.NSample=NSample
-    
+        
+        self.YInit=YInit
+        if self.surrogate:
+            if XInit is None:
+                self.XInit=lhs(NSurrogate, self.dim)*(self.ub-self.lb)+self.lb
+                
     def analyze(self):
         '''
         main procedure
@@ -24,26 +31,38 @@ class DELTA_TEST():
         NSample=self.NSample
         
         X_seq=(ub-lb)*lhs(NSample, NInput)+lb
-        Y_seq=self.evaluator(X_seq)
         
-        self.X_seq=X_seq; self.Y_seq=Y_seq
+        if self.surrogate:
+            if self.XInit is None:
+                self.XInit=lhs(self.NSurrogate, self.dim)*(self.ub-self.lb)+self.lb
+                self.YInit=self.evaluate(self.XInit)
+            self.surrogate.fit(self.XInit, self.YInit)
+            Y_seq=self.surrogate.predict(X_seq)
+        else:
+            Y_seq=self.evaluate(X_seq)
+            
+        Y_seq_scale=scale.fit_transform(Y_seq)
+        
+        self.X_seq=X_seq; self.Y_seq=Y_seq_scale
         bin_ga=Binary_GA(self.cal_delta, NInput)
-        a=bin_ga.run()    
-        b=1
+        best_paras, best_value, history_paras, _=bin_ga.run()
+        
+        idx=[index for index, value in enumerate(best_paras) if value == 1]
+
+        score=np.sum(history_paras, axis=0)
+        
+        return idx, score[0, idx]
     
     def cal_delta(self, exclude_feature_list):
         X=np.copy(self.X_seq)
         y=np.copy(self.Y_seq)
-        exclude_feature = [index for index, value in enumerate(exclude_feature_list) if value == 1]
+        exclude_feature = [index for index, value in enumerate(exclude_feature_list) if value == 0]
         if exclude_feature is not None:
-            # 排除指定的特征
             X = np.delete(X, exclude_feature, axis=1)
-        
-        # 计算所有样本点之间的距离
+            
         distances = cdist(X, X)
         np.fill_diagonal(distances, np.inf)
         
-        # 为每个样本找到最近的n个邻居
         neighbors_indices = np.argsort(distances, axis=1)[:, :self.n_neighbors]
         
         # 计算Delta值
