4ebf1b0e132ae0506b382c30735ffff01b6c0fda
Smasky <492109831@qq.com>
2024-04-18 21:26:40 +0800

    'wheel'

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ numpy_inc = numpy.get_include()
 pybind11_inc = pybind11.get_include()
 # cython扩展模块
 cython_extensions = [
-    # Extension("UQPyL.surrogates.mars_._types", ["UQPyL/surrogates/mars_/_types.pyx"], include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._util", ["UQPyL/surrogates/mars_/_util.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._forward", ["UQPyL/surrogates/mars_/_forward.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._record", ["UQPyL/surrogates/mars_/_record.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._basis", ["UQPyL/surrogates/mars_/_basis.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._pruning", ["UQPyL/surrogates/mars_/_pruning.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._qr", ["UQPyL/surrogates/mars_/_qr.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    # Extension("UQPyL.surrogates.mars_._knot_search", ["UQPyL/surrogates/mars_/_knot_search.pyx"],include_dirs=[numpy_inc], extra_compile_args=['/w'],),
-    Extension('UQPyL.surrogates.lasso_.lasso_fast', sources=[str(Path('UQPyL/surrogates/lasso_/lasso_fast.pyx'))], include_dirs=[numpy_inc], extra_compile_args=['/w'],)
+    Extension("UQPyL.surrogates.mars_._types", ["UQPyL/surrogates/mars_/_types.pyx"], include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._util", ["UQPyL/surrogates/mars_/_util.pyx"],include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._forward", ["UQPyL/surrogates/mars_/_forward.pyx"],include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._record", ["UQPyL/surrogates/mars_/_record.pyx"],include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._basis", ["UQPyL/surrogates/mars_/_basis.pyx"],include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._pruning", ["UQPyL/surrogates/mars_/_pruning.pyx"],include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._qr", ["UQPyL/surrogates/mars_/_qr.pyx"],include_dirs=[numpy_inc]),
+    Extension("UQPyL.surrogates.mars_._knot_search", ["UQPyL/surrogates/mars_/_knot_search.pyx"],include_dirs=[numpy_inc]),
+    Extension('UQPyL.surrogates.lasso_.lasso_fast', sources=[str(Path('UQPyL/surrogates/lasso_/lasso_fast.pyx'))], include_dirs=[numpy_inc],)
 ]
 #pybind11扩展模块
 pybind11_extensions = [
