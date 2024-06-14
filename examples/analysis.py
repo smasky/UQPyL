6c239819282289b66bbe0162757377e10e3fa4a5
Smasky <492109831@qq.com>
2024-06-14 20:48:54 +0800

    '0614'

--- examples/analysis.py
+++ examples/analysis.py
@@ -1,18 +1,45 @@
 import pandas as pd
 import os
 import numpy as np
+from scipy.stats import kruskal, mannwhitneyu
 os.chdir('./examples')
 benchmarks={ 1: "Sphere", 2: "Schwefel_2_22", 3: "Schwefel_1_22", 4: "Schwefel_2_21", 5: "Schwefel_2_26",
              6: "Rosenbrock", 7: "Step", 8: "Quartic", 9: "Rastrigin", 10: "Ackley", 11: "Griewank", 12: "Trid", 
              13: "Bent_Cigar", 14: "Discus", 15: "Weierstrass" }
-surrogate=["RBF-CB", "RBF-LN", "RBF-TPS", "RBF-MLP", "RBF-GASS"]
+surrogate=["RBF-CB", "RBF-LN", "RBF-TPS", "RBF-MLP"]
 database = pd.read_excel("./database.xlsx", index_col=0)
-dimensions=[5, 15, 30, 50]
-samples=[100, 300, 500]
+dimensions=[10, 20, 30, 50]
+samples=[100, 200, 300, 500]
 
-for sur in surrogate:
+# for sur in surrogate:
+#     for dim in dimensions:
+#         for sample in samples:
+#             res=database[(database['surrogate'] == sur) & (database['dimensions'] == dim) & (database['samples'] == sample)]['r_square'].to_numpy()
+#             res[res<0]=0
+#             count=np.sum(res==0)
+#             print(sur, "dim:", dim,"sample:", sample,"mean:", np.mean(res), "std:", np.std(res))
+   
+for key, problem in benchmarks.items():
+    score_2={"RBF-CB": 0, "RBF-LN": 0, "RBF-TPS": 0, "RBF-MLP": 0}
+    score_1={"RBF-CB": 0, "RBF-LN": 0, "RBF-TPS": 0, "RBF-MLP": 0}
     for dim in dimensions:
         for sample in samples:
-            res=database[(database['surrogate'] == sur) & (database['dimensions'] == dim) & (database['samples'] == sample)]['rank_score'].to_numpy()
-            print(sur, "dim:", dim,"sample:", sample,"mean:", np.mean(res), "std:", np.std(res))
+            first=database[(database['surrogate'] == surrogate[0]) & (database['dimensions'] == dim) & (database['samples'] == sample) &(database['problem']==problem)]['r_square'].to_numpy()
+            name=[surrogate[0]]
+            for i in range(1, len(surrogate)):
+                second=database[(database['surrogate'] == surrogate[i]) & (database['dimensions'] == dim) & (database['samples'] == sample) &(database['problem']==problem)]['r_square'].to_numpy()
+                stat, p_value = mannwhitneyu(first, second, alternative='two-sided')
+                if p_value > 0.20:
+                    name.append(surrogate[i])
+                elif np.mean(second)>np.mean(first):
+                    name=[surrogate[i]]
+                    first=second
+            if(len(name)==1):
+                score_1[name[0]]+=1
+            else:
+                for n in name:
+                    score_2[n]+=1
+    print(problem)
+    print(score_1)
+    print(score_2)
             
--- examples/comparsion.py
+++ examples/comparsion.py
@@ -22,8 +22,8 @@ benchmarks={1: Sphere, 2: Schwefel_2_22, 3: Schwefel_1_22, 4: Schwefel_2_21, 5: 
             6: Rosenbrock, 7:Step, 8: Quartic, 9:Rastrigin, 10:Ackley, 11:Griewank, 12:Trid, 
             13:Bent_Cigar, 14: Discus, 15:Weierstrass}
 
-dimensions=[5, 15, 30, 50]
-samples=[50, 150, 300, 500]
+dimensions=[10, 20, 30, 50]
+samples=[100, 200, 300, 500]
 # columns = ['problem', 'surrogate', 'dimensions', 'samples', 'r_square', 'rank_score']
 database = pd.read_excel("./database.xlsx", index_col=0)
 #----------------------RBF-----------------------------#
@@ -35,7 +35,6 @@ for id, func in benchmarks.items():
     for dim in dimensions:
         for sample in samples:
             for time in range(20):
-                
                 problem=func(dim)
                 lhs=LHS('classic', problem=problem)
                 
@@ -45,14 +44,14 @@ for id, func in benchmarks.items():
                 test_X=lhs.sample(sample, problem.n_input, random_seed=seed_test[index, time])
                 test_Y=problem.evaluate(test_X)
 
-                surrogate=RBF(kernel=Gaussian())
+                surrogate=RBF(kernel=Multiquadric())
                 surrogate.fit(train_X, train_Y)
                 pre_Y=surrogate.predict(test_X)
                 r2=r_square(test_Y, pre_Y)
                 rank=rank_score(test_Y, pre_Y)
                 
-                database.loc[len(database)]=[problem.__class__.__name__, 'RBF-GASS', index, time,dim, sample, r2, rank]
-        index+=1
+                database.loc[len(database)]=[problem.__class__.__name__, 'RBF-MLP', index, time,dim, sample, r2, rank]
+            index+=1
 database.to_excel("./database.xlsx")
             
 
