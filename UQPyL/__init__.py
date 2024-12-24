dc709e5f7a04a3e03061e3c45fccd468ae89eee5
Smasky <492109831@qq.com>
2024-12-24 09:43:09 +0800

    '1224'

--- UQPyL/__init__.py
+++ UQPyL/__init__.py
@@ -1,6 +1,6 @@
 from . import problems, surrogates, optimization, sensibility, DoE, utility
 
-__version__ = "2.0.9"
+__version__ = "2.0.10"
 __author__ = "wmtSky"
 
 __all__=[
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extensions=cythonize(cython_extensions, compiler_directives={'cdivision': True, 
 setup(
     name="UQPyL",
     author="wmtSky",
-    version="2.0.9",
+    version="2.0.10",
     author_email="wmtsky@hhu.edu.cn",
     ext_modules=extensions,  
     packages=find_packages(),
