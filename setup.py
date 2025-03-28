5f4888823c4bad62a454ee98b8f111981a8148c6
Smasky <492109831@qq.com>
2025-03-28 21:22:08 +0800

    '0328'

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
     author_email="wmtsky@hhu.edu.cn",
     ext_modules=extensions,  
     packages=find_packages(),
-    license="MIT",
     description="A python package for parameter uncertainty quantification and optimization",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",  # 如果是Markdown格式
